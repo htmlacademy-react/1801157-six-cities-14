0404596da253645b088b7bf7ffe2aa39532c959d
CamONE1 <kammnsn@me.com>
2023-10-15 11:18:55 +0300

    created components and pages

---
+++ src/components/app/app.tsx
@@ -0,0 +1,13 @@
+import MainPage from "../../pages/main-page/main-page";
+
+type AppScreenProps = {
+  placesCount: number
+}
+
+export default function App({placesCount}: AppScreenProps): JSX.Element {
+  return (
+    <MainPage 
+      placesCount = {placesCount}
+    />
+  );
+}
